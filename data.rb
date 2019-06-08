87d002fe13ade274a740e133b1c7d034193d6e1a
active-attribute-9953 <mychal.thigpen@gmail.com>
2019-06-08 17:42:58 +0000

    Done.

---
+++ data.rb
@@ -0,0 +1,43 @@
+#This is how to create variables
+age = 28
+height = 160
+
+#This is how to use variables
+puts height + age
+#everything on this line is not a code
+
+#working with String
+first_name = "Ann"
+last_name = "Duong"
+#puts first_name + " " + last_name
+
+#puts "Ann".length
+#puts last_name.downcase
+puts last_name.upcase
+
+teacher = "Ann"
+puts teacher
+teacher = "Trevor"
+puts teacher
+teacher = "Joseph"
+puts teacher
+
+teachers = ["Ann", "Trevor", "Joseph"]
+puts teachers
+
+#How to index an Array
+puts teachers[0]
+puts teachers[2]
+puts teachers[1]
+#Indexing starts at number 0
+
+#puts teacher
+
+#What is a boolean? is a true or false 
+female = true 
+tall = false 
+
+puts tall 
+
+#What is a nil 
+husband = nil 
---
+++ practice.rb
@@ -0,0 +1,10 @@
+famous_cats = ["lil ' bub", "grumpy cat", "Maru"]
+
+famous_cats << "nala cat"
+
+#puts famous_cats
+
+my_array = "Mychal".reverse
+#puts my_array
+random_numbers = [ 2, 5, 6, 8, 30050]
+puts random_numbers
